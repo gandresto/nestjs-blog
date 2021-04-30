ddbe9185faf8b6838274db1a06b0a33245a1da5b
gandresto-tecbot <gandresto@outlook.com>
2021-04-30 10:38:30 -0500

    Corrijo dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { GraphQLModule } from '@nestjs/graphql';
-// import { join } from 'node:path';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
-    // GraphQLModule.forRoot({
-    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    //   sortSchema: true,
-    // }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    MongooseModule.forRoot(''),
+    UsersModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],

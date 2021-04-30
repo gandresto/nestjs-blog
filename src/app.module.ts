4a81c6e2aa1ed0d803c81540a9a8d4703d9e77f1
gandresto-tecbot <gandresto@outlook.com>
2021-04-30 11:02:36 -0500

    Agrego variables de entorno y su configuración

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,16 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
-    MongooseModule.forRoot(''),
+    MongooseModule.forRoot(process.env.MONGODB_CONN_STR),
     UsersModule,
     PostsModule,
   ],
